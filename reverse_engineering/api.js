0d2abbabbb509430e23aa6636d6753cba5a1b148
DolynskiyEduard <dolynskiy.eduard@gmail.com>
2017-10-23 15:53:51 +0300

    add method for fetch request via REST and method for get keyspaces list

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -10,5 +10,51 @@ module.exports = {
 	},
 
 	testConnection: function(connectionInfo, logger, cb){
+	},
+
+	getDbCollectionsNames: function(connectionInfo, logger, cb) {
 	}
+}
+
+function getKeyspacesList(){
+	let query = '/api/v1/keyspaces';
+	// return fetchRequest(query).then(res => {
+	// 	return res;
+	// });
+	return ['Keyspace1', 'Keyspace2']
+}
+
+function getRequestOptions(){
+	let date = new Date().toUTCString();
+	let authToken = '';
+
+	let headers = {
+		'Cache-Control': 'no-cache',
+		'authorization': authToken,
+		'x-ms-date': date,
+		'x-ms-version': '2017-01-19',
+		'Accept': 'application/json'
+	};
+
+	return {
+		'method': 'GET',
+		'headers': headers
+	};
+}
+
+function fetchRequest(query){
+	let options = getRequestOptions(REST, config);
+
+	return fetch(query, options)
+		.then(res => {
+			return res.text();
+		})
+		.then(body => {
+			body = JSON.parse(body);
+			console.log(body);
+			return body;
+		})
+		.catch(err => {
+			console.log(err);
+		});
 }
