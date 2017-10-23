018887bc8a11bfca2aa99b30444d480fe209c84a
DolynskiyEduard <dolynskiy.eduard@gmail.com>
2017-10-23 18:24:06 +0300

    add functionality for gect collection names (from mocked data)

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -13,16 +13,13 @@ module.exports = {
 	},
 
 	getDbCollectionsNames: function(connectionInfo, logger, cb) {
+		getKeyspacesList((err, res) => {
+			let keyspacesNames = res.data.map(ks => ks.name);
+			handleKeyspace(connectionInfo, keyspacesNames, cb);
+		};
 	}
 }
 
-function getKeyspacesList(){
-	let query = '/api/v1/keyspaces';
-	// return fetchRequest(query).then(res => {
-	// 	return res;
-	// });
-	return ['Keyspace1', 'Keyspace2']
-}
 
 function getRequestOptions(){
 	let date = new Date().toUTCString();
@@ -57,4 +54,130 @@ function fetchRequest(query){
 		.catch(err => {
 			console.log(err);
 		});
+}
+
+function handleKeyspace(connectionInfo, keyspacesNames, cb){
+	async.map(keyspacesNames, (keyspaceName, keyspaceItemCallback) => {
+		readKeyspaceByName(keyspaceName, (err, keySpace) => {
+			if(err){
+				console.log(err);
+				logger.log('error', err);
+				keyspaceItemCallback(err);
+			} else {
+				let size = /*getSampleDocSize(amount, connectionInfo.recordSamplingSettings) ||*/ 1000;
+
+				getCollectionsList(keyspaceName, size, (err, collectionList) => {
+					if(err){
+						console.log(err);
+						return keyspaceItemCallback(err);
+					} else {
+						let collectionNames = collectionList.data.map(coll => coll.name);
+						let dataItem = prepareConnectionDataItem(keyspaceName, collectionNames);
+						return keyspaceItemCallback(err, dataItem);
+					}
+				});
+			}
+		});
+	}, (err, items) => {
+		if(err){
+			console.log(err);
+			logger.log('error', err);
+		}
+		return cb(err, items);
+	});
+}
+
+function getKeyspacesList(cb){
+	let query = '/api/v1/keyspaces';
+	// return fetchRequest(query).then(res => {
+	// 	return res;
+	// });
+	
+	let data = {
+	  "apiVersion": "v1",
+	  "kind": "KeyspaceList",
+	  "data": [{name: 'Keyspace1'}, {name: 'Keyspace2'}]
+	};
+
+	return cb(null, data);
+}
+
+function readKeyspaceByName(keyspaceName, cb){
+	let query = `/api/v1/keyspaces/${keyspaceName}`;
+	// return fetchRequest(query).then(res => {
+	// 	return res;
+	// });
+
+	let data = {
+	  "apiVersion": "v1",
+	  "kind": "KeyspaceList",
+	  "data": { name: keyspaceName }
+	};
+
+	return cb(null, data);
+}
+
+function getCollectionsList(keyspaceName){
+	let query = `/api/v1/keyspaces/${keyspaceName}`;
+	// return fetchRequest(query).then(res => {
+	// 	return res;
+	// });
+	
+	let data = {
+		"apiVersion": "v1",
+		"kind": "CollectionList",
+		"data": [
+			{
+				"id": 0,
+				"name": "string",
+				"keyType": "LONG",
+				"children": [
+					null
+				],
+				"indices": [
+					{
+						"name": "string",
+						"fields": [
+							{
+								"name": "string",
+								"order": "Asc"
+							}
+						],
+						"cardinality": "Unique"
+					}
+				]
+			},
+			{
+				"id": 0,
+				"name": "string",
+				"keyType": "LONG",
+				"children": [
+					null
+				],
+				"indices": [
+					{
+						"name": "string",
+						"fields": [
+							{
+								"name": "string",
+								"order": "Asc"
+							}
+						],
+						"cardinality": "Unique"
+					}
+				]
+			}
+		]
+	};
+
+	return cb(null, data);
+}
+
+function prepareConnectionDataItem(keyspaceName, collectionNames){
+	let connectionDataItem = {
+		dbName: keyspaceName,
+		dbCollections: collectionNames
+	};
+
+	return connectionDataItem;
 }
